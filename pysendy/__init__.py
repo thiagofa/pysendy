e378ce6f267cce31f0c81fd90057486ceaf263a1
Thiago Faria de Andrade <thiagofa@gmail.com>
2013-12-07 16:02:41 -0200

    Pypi upload instructions fixed

--- pysendy/__init__.py
+++ pysendy/__init__.py
@@ -8,7 +8,7 @@ Release logic:
 1. Remove "dev" from current.
 2. git commit
 3. git tag <version>
-4. push to pypi (python setup.py sdist bdist upload) + push to github
+4. push to pypi (python setup.py sdist upload) + push to github
 5. bump the version, append '.dev0'
 6. git commit
 7. push to github (to avoid confusion)
