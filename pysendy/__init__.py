285aae6af965d99ba8ad7fb369af80f24711df80
Thiago Faria de Andrade <thiagofa@gmail.com>
2013-12-07 15:53:50 -0200

    Bump up version

--- pysendy/__init__.py
+++ pysendy/__init__.py
@@ -8,7 +8,7 @@ Release logic:
 1. Remove "dev" from current.
 2. git commit
 3. git tag <version>
-4. push to pypi + push to github
+4. push to pypi (python setup.py sdist bdist upload) + push to github
 5. bump the version, append '.dev0'
 6. git commit
 7. push to github (to avoid confusion)
@@ -17,4 +17,4 @@ Release logic:
 __author__ = 'Thiago Faria de Andrade'
 __copyright__ = 'Copyright 2013, Thiago Faria de Andrade'
 __credits__ = ['Thiago Faria de Andrade',]
-__version__ = '0.0.3'
+__version__ = '0.0.4.dev0'
